43583292e9f36be837625764366a7765ed983e74
peejayb1996 <percian0296@gmail.com>
2017-10-11 01:12:46 +0800

    Create Error.php

---
+++ App/Defaults/Error.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ *
+ * CHKN Framework PHP
+ * Copyright 2015 Powered by Percian Joseph C. Borja
+ * Created May 12, 2015
+ * Class Error
+ * Holds the function that will load error page when there is no class or function found
+ */
+class chknError{
+	public function error_page(){
+      $template = file_get_contents(DEFAULT_URL.'view/defaults/error.tpl');
+      $template = str_replace('{ERROR_PATH}',DEFAULT_URL,$template);
+      echo $template;
+	}
+}
